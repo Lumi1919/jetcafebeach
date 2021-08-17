e66658e9f1221d55f98c2ab4428d9981493cc90f
Lumi <shensanlumi@gmail.com>
2021-08-17 15:51:44 +0100

    commit message

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jetcafebeach.wsgi --log-file -
--- jetcafebeach/settings.py
+++ jetcafebeach/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (BASE_DIR, 'static',)
